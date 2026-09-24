866ff705941a35ae25f20db7ac498dd552fa4e51
KeerthanKKK <skillstack.keerthan@gmail.com>
2026-09-24 20:15:35 +0530

    Changed the version to 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const PORT = process.env.PORT ?? 8080
 
 app.get('/', (req, res) => {
-    return res.json({ msg: 'Hello from the server --version1' })
+    return res.json({ msg: 'Hello from the server --version2' })
 })
 
 app.listen(PORT, () => {
